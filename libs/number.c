9a54a85469eb43158ac185573441342f026fda30
jinsuk5 <jinsuk5@naver.com>
2015-11-06 01:24:24 -0800

    sook-number2

--- libs/number.c
+++ libs/number.c
@@ -1,17 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
+
+char in[100];
+
+int getche(void)
+{
+   struct termios oldattr, newattr;
+   int ch;
+   tcgetattr(STDIN_FILENO, &oldattr);
+   newattr = oldattr;
+   newattr.c_lflag &= ~(ICANON);
+   tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+   ch = getchar();
+   tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+   return ch;
+}
+
 void changeToNum(char a)
 {
    char ch;
+   int _where;
    in[_where] = a;
 
    if (in[_where] == '0')
-      printf(" ");
+      printf("space\n");
    else
    {
       while (1)
       {
          ch = getche();
-          _where++;
-          in[_where] = ch;
+	 if(ch == '+')
+	 {
+		printf("change\n");
+		return;
+	 }
+	 if(ch == '-')
+	 {
+		printf("delete\n");
+	 }
+	 else
+	 {
+         	_where++;
+         	in[_where] = ch;
+	 }
       }
    }
 }
@@ -21,7 +54,10 @@ int main()
 	char ch;
 	while(1)
 	{
+		
 		ch = getche();
 		changeToNum(ch);
+		
+		
 	}
 }
