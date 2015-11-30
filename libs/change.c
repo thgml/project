dfd96fc282b2b967c257a6f02f1a16ee1bf57a47
Jinsook Oh <jins01060212@gmail.com>
2015-11-30 14:58:04 -0800

    sook-korean5

--- libs/change.c
+++ libs/change.c
@@ -6,22 +6,27 @@
 void change()
 {
 	int j = 0;
-	int _where;
-	char in[1000];
-	
-	if(1) // flag condition
+	if (flag == 2 || flag == 3)
 	{
-		if(1) // current
-			_where--;
+		if (now == 1)
+		_where--;
 	}
 
-	// display
-	for(j=0; j<=_where; j++)
+	//display
+	for (j = 0; j <= _where; j++)
+		printf("%s", unicode_to_utf8(in[j]));
+	if (flag == 1)
+		flag = 2;
+	else if (flag == 2)
+		flag = 3;
+	else if (flag == 3)
+		flag = 1;
+
+	//display
+	for (j = 0; j <= _where; j++)
 		printf("%s", unicode_to_utf8(in[j]));
 
-	// display
-	for(j=0; j<=_where; j++)
-		printf("%s", unicode_to_utf8(in[j]));	
+	return;
 }
 
 int main()
