6aadd5554f729154a48bebf66f987d917fd6b303
jinsuk5 <jinsuk5@naver.com>
2015-12-06 11:31:18 -0800

    sook-main5

--- libs/main.c
+++ libs/main.c
@@ -88,6 +88,55 @@ void changeToKorean(char a)
    }
 }
 
+void display(int *flag, int *capsLock)
+{
+	int y, x, z, cnt = 6;
+	for (y = 2; y <= 8; y += 3)
+	{
+		for (x = 3; x <= 15; x += 6)
+		{
+			gotoxy(x, y);
+			printf("%d", ++cnt);
+			gotoxy(x + 1, y + 1);
+			if (flag == 1)
+			{
+				for (z = 0; z <= 2; z += 1)
+					printf("str");
+			}
+			if (flag == 2 && capsLock == 0)
+			{
+				for (z = 0; z <= 3; z += 1)
+					printf("str");
+
+			}
+			if (flag == 2 && capsLock == 1)
+			{
+				for (z = 0; z <= 3; z += 1)
+					printf("str");
+			}
+			if (cnt == 9)
+				cnt = 3;
+			else if (cnt == 6)
+				cnt = 0;
+		}
+	}
+	gotoxy(3, 11);
+	printf("0");
+	gotoxy(4, 12);
+	if (flag == 1)
+		printf("str str");
+	else if (flag == 2 && capsLock == 0)
+		printf("Large");
+	else if (flag == 2 && capsLock == 1)
+		printf("Small");
+	gotoxy(15, 11);
+	printf(".");
+	gotoxy(16, 12);
+	if (flag == 1 || flag == 2 || flag == 3)
+		printf("SP");
+	gotoxy(1, 15);
+}
+
 void changeToNum(char a)
 {
    char ch;
--- libs/main.c~
+++ libs/main.c~
@@ -46,6 +46,48 @@ void change()
 	return;
 }
 
+void changeToKorean(char a)
+{
+	char ch;
+	int cons_flag = 0;
+	int vowel_sum = 0;
+	int i = 0;
+	int j = 0;
+	int _where = 0;  //////////////
+	char in[_where] = a;
+
+
+	if (in[_where] == '.')
+		//space
+	else
+	{
+		//cons_flag = consonant
+		while (1)
+		{
+			ch = getche();
+			if (ch == '+')
+			{
+				//change
+				return;
+			}
+			if (ch == '-')
+			{
+				backspace();
+				continue;
+			}
+			if (ch == '.')
+			{
+				space();
+				continue;
+			}
+         else if (ch == '7' || ch == '8' || ch == '9')
+         {
+		//
+         }
+      }
+   }
+}
+
 void changeToNum(char a)
 {
    char ch;
